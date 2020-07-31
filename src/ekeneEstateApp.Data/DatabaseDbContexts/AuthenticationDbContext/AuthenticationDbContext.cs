4c9e2087da1031d69d6aff2e31da9586b04c9471
Ekene Samuel Anolue <kenelight4u@gmail.com>
2020-07-31 16:24:45 +0100

    added authenticationdbContext

--- src/ekeneEstateApp.Data/DatabaseDbContexts/AuthenticationDbContext/AuthenticationDbContext.cs
+++ src/ekeneEstateApp.Data/DatabaseDbContexts/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,7 +1,14 @@
+using ekeneEstateApp.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
 namespace ekeneEstateApp.Data.AuthenticationDbContext
 {
-    public class AuthenticationDbContext
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
-        
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext>options) : base(options)
+        {
+
+        }
     }
 }
