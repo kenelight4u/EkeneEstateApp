5d6b673c92fd305cb6b6ee75bfecbfc664cf51d7
Ekene Samuel Anolue <kenelight4u@gmail.com>
2020-08-01 15:30:37 +0100

    fixing azure

--- src/ekeneEstateApp.Web/Startup.cs
+++ src/ekeneEstateApp.Web/Startup.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using ekeneEstateApp.Data.DatabaseDbContexts.ApplicationDbContext;
 using ekeneEstateApp.Data.DatabaseDbContexts.AuthenticationDbContext;
 using ekeneEstateApp.Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
