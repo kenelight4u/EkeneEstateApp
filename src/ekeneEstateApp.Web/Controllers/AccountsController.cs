bab382ee5bbaa4c1434d5cb46cb3a7960c440583
Ekene Samuel Anolue <kenelight4u@gmail.com>
2020-08-04 07:40:42 +0100

    validated our models

--- src/ekeneEstateApp.Web/Controllers/AccountsController.cs
+++ src/ekeneEstateApp.Web/Controllers/AccountsController.cs
@@ -1,17 +1,37 @@
+using System;
+using ekeneEstateApp.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ekeneEstateApp.Web.Controllers
 {
     public class AccountsController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid) return View();
+
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid) return View();
+            
+            throw new NotImplementedException();
         }
     }
 }
