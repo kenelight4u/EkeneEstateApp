4fc979d51722cf33fc2f04db66362d3cddb61562
Ekene Samuel Anolue <kenelight4u@gmail.com>
2020-08-07 15:05:12 +0100

    mistake trying to update

--- src/ekeneEstateApp.Data/Migrations/ApplicationDb/20200807131312_added colums to property entity.Designer.cs
+++ src/ekeneEstateApp.Data/Migrations/ApplicationDb/20200807131312_added colums to property entity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ekeneEstateApp.Data.DatabaseDbContexts.ApplicationDbContext;
 
 namespace ekeneEstateApp.Data.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200807131312_added colums to property entity")]
+    partial class addedcolumstopropertyentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
